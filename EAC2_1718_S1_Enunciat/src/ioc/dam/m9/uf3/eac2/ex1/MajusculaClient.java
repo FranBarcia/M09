7e78346eeed29bae9479b05899ff0593b469ae04
fbarcia <fbarcia@DESKTOP-CQEL88O>
2017-11-17 00:01:55 +0100

    Ex1 y Ex2 acabados

--- EAC2_1718_S1_Enunciat/src/ioc/dam/m9/uf3/eac2/ex1/MajusculaClient.java
+++ EAC2_1718_S1_Enunciat/src/ioc/dam/m9/uf3/eac2/ex1/MajusculaClient.java
@@ -1,12 +1,48 @@
 package ioc.dam.m9.uf3.eac2.ex1;
 
+import static com.sun.xml.internal.messaging.saaj.packaging.mime.util.ASCIIUtility.getBytes;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 /**
  *
  * @author acanela
  */
 public class MajusculaClient {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // Variables per la connexió
+        InetAddress servidor = InetAddress.getByName("localhost");
+        DatagramSocket socol = new DatagramSocket();
+        byte[] dadesRebudes = new byte[1024];
+        byte[] dadesEnviades;
+        
+        // Variable per tractar les dades a enviar
+        Scanner llegirText = new Scanner(System.in);
+        String missatgeEnviat;
+        String missatgeRebut;
+        
+        System.out.println("Introdueix un text qualsevol: ");
+        missatgeEnviat = llegirText.nextLine();
+        
+        System.out.println("Enviaré aixo: "+missatgeEnviat+"\n.........................");
+        //creació del paquet per enviar la resposta
+        dadesEnviades = getBytes(missatgeEnviat);
+        DatagramPacket paquet = new DatagramPacket(dadesEnviades, dadesEnviades.length, servidor, 9999);
+        //enviament de la resposta
+        socol.send(paquet);
+        
+        paquet = new DatagramPacket(dadesRebudes, 1024);
+        socol.receive(paquet);
+        missatgeRebut = new String(paquet.getData(), 0, paquet.getLength(), "ISO-8859-15");
+        System.out.println("M'han tornat això: "+missatgeRebut);
+        System.out.println("Ja ni ha prou de conversar, ara tanco la connexió.");
+        
+        socol.close();
+            
+        
         /*
         Aquesta classe només tindrà el mètode main que és el que heu d'implementar.
         Aquest mètode simplement:
--- EAC2_1718_S1_Enunciat/src/ioc/dam/m9/uf3/eac2/ex1/MajusculaServidor.java
+++ EAC2_1718_S1_Enunciat/src/ioc/dam/m9/uf3/eac2/ex1/MajusculaServidor.java
@@ -1,25 +1,59 @@
 package ioc.dam.m9.uf3.eac2.ex1;
 
+import static com.sun.xml.internal.messaging.saaj.packaging.mime.util.ASCIIUtility.getBytes;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  *
  * @author albert
  */
 public class MajusculaServidor {
 
-    public void servei() {
-        /*
-        Aquesta classe estarà sempre “escoltant”. 
-        Quan rebi un text d'un client:
-            - el convertirà a majúscules
-            - li retornarà. 
-        Tot ho farà a través del mètode servei()
-        */
-    }
+    public void servei() throws Exception{
+        // Variables per la connexió
+        DatagramSocket socol = new DatagramSocket(9999);
+        byte[] dadesRebudes = new byte[1024];
+        byte[] dadesEnviades;
+        InetAddress clientIP;
+        Integer clientPort;
+        
+        String missatge;
 
-    public static void main(String[] args) {
+        //el servidor atén el port indefinidament
+        while(true){
+            //creació del paquet per rebre les dades
+            DatagramPacket paquet = new DatagramPacket(dadesRebudes, 1024);
+            System.out.println("Esperant dades\n.........................");
+            socol.receive(paquet);
+            System.out.println("Dades rebudes.");
+            //processament de les dades rebudes i obtenció de la resposta
+            missatge = new String(paquet.getData(), 0, paquet.getLength(), "ISO-8859-15");
+            System.out.println("He rebut: "+missatge);
+            missatge = missatge.toUpperCase();
+            dadesEnviades = getBytes(missatge);
+            System.out.println("Enviaré: "+missatge);
+            /*
+            private byte[] processData(byte[] data, int length) {
+            //procés diferent per cada aplicació
+
+            }
+            */
+            //obtenció de l'adreça i el port del client
+            clientIP = paquet.getAddress();
+            clientPort = paquet.getPort();
+            //creació del paquet per enviar la resposta
+            paquet = new DatagramPacket(dadesEnviades, dadesEnviades.length, clientIP, clientPort);
+            //enviament de la resposta
+            socol.send(paquet);
+            System.out.println("Missatge '"+missatge+"' enviat.");
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
         MajusculaServidor majuscula = new MajusculaServidor();
 
         majuscula.servei();
-
     }
 }
